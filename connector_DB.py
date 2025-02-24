9226a9dca0e9caed04a3d97ee22debc9f568d208
AdiCohenTzedek <adic206@gmail.com>
2025-02-24 19:25:28 +0200

    requirements file and readMe

--- connector_DB.py
+++ connector_DB.py
@@ -14,48 +14,6 @@ workouts_col = lifted_db['workouts']
 workouts_registration_col = lifted_db['workouts_registration']
 
 
-# # פונקציה להוספת משתמש
-# def add_user(data):
-#
-#     # יצירת נתוני המשתמש
-#     user_data = {
-#         "email": data.get("email"),
-#         "firstName": data.get("firstName"),
-#         "lastName": data.get("lastName"),
-#         "age": int(data.get("age", 0)),  # המרת גיל למספר
-#         "phone": data.get("phone")
-#     }
-#
-#     result = users_col.insert_one(user_data)
-#     return result.inserted_id
-#
-
-# פונקציה לבדוק אם משתמש קיים
-def user_exists(email):
-    collection = users_col["users"]
-    return collection.find_one({"email": email}) is not None
-
-
-# פונקציה להציג את כל הנתונים מאוסף
-def get_all_from_collection(collection_name):
-    collection = users_col[collection_name]
-    return list(collection.find())
-
-
-# פונקציה לעדכון נתון באוסף
-def update_in_collection(collection_name, query, new_values):
-    collection = users_col[collection_name]
-    result = collection.update_one(query, {"$set": new_values})
-    return result.modified_count
-
-
-# פונקציה למחיקת נתון מאוסף
-def delete_from_collection(collection_name, query):
-    collection = users_col[collection_name]
-    result = collection.delete_one(query)
-    return result.deleted_count
-
-
 def add_workout(workout_type, trainer, time, day):
     workout_data = {
         "workout_type": workout_type,
